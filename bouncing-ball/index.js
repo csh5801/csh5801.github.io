6ac897f059597cc2916fffc27ffa392f86fd6059
Daniel <enddl5801@naver.com>
2021-01-06 18:58:28 +0900

    Started ball game

---
+++ bouncing-ball/index.js
@@ -0,0 +1,39 @@
+    const config = {
+        type: Phaser.AUTO,
+        pixelArt: true,
+        width: 600,
+        height: 400,
+        physics: {
+            default: "arcade" ,
+            arcade: {
+                gravity: { y: 0 } ,
+                debug: false
+            }
+        } ,
+        scene: { preload, create, update }
+    }
+
+    const game = new Phaser.Game(config)
+
+    let ball;
+    let ground;
+
+    function preload() {
+        this.load.image("ball", "assets/ball.png")
+        this.load.image("spike", "assects/spike.png")
+        this.load.image("ground", "assects/ground.png")
+    }
+
+    function create() {
+        ball = this.physics.add.sprite(100, 100, "ball")
+        ground = this.physics.add.sprite(100, 300, "ground") 
+        
+        ground.setImmovable(true)
+        ball.body.gravity.y = 200;
+        ball.setBounce(1)
+        
+        this.physics.add.collider(ground, ball)
+    }
+        
+     
+    function update() {}
