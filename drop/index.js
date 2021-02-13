969e5297fcee25a91e0633c7f04a9c1bf28086dd
Daniel <enddl5801@naver.com>
2021-02-13 21:00:50 +0900

    added drop game

--- drop/index.js
+++ drop/index.js
@@ -17,6 +17,7 @@ const game = new Phaser.Game(config)
 
 let ball;
 let platforms;
+let ballOnPlatform = false;
 
 function topPlatform() {
     let topY = Infinity;
@@ -42,32 +43,57 @@ function bottomY() {
     return bottom
 }
 
+function hitPlatform(ball, platform) {
+  if (ballOnPlatform) {
+      return
+  }
+
+  ballOnPlatform = true
+
+  if(platform !== topPlatform()) {
+     this.scene.restart()
+  }
+}
+
 function preload() {
     this.load.image("ball", "assets/ball.png")
     this.load.image("platform", "assets/platform.png")
 }
 
 function create() {
-    ball = this.physics.add.sprite(100, 100, "ball")
+    ball = this.physics.add.sprite(375, 100, "ball")
     ball.setGravityY(1500)
+    ball.setFrictionX(1)
 
     this.cameras.main.startFollow(ball)
 
     platforms = this.physics.add.group()
 
-    this.physics.add.collider(ball, platforms)
+    this.physics.add.collider(ball, platforms, hitPlatform, null, this)
+     
+    let startPlatform = platforms.create(375, 160, "platform")
+    startPlatform.setImmovable(true)
 
 for (i = 0; i < 10; i++) {
-    const x = 100;
+    const x = Phaser.Math.Between(150, 600);
     const y = 300 + i * 50
      let platform = platforms.create(x, y, "platform")
      platform.setImmovable(true)
+     platform.setVelocityX(Phaser.Math.Between(100, 200))
+     platform.setFrictionX(1)
     }
 
     this.input.on("pointerdown", () => {
         const y = bottomY() + 50      
         topPlatform().setY(y)
+        ballOnPlatform = false
     })
 }
 
-function update() {}
+function update() {
+    platforms.getChildren().forEach((platform) => {
+        if (platform.getBounds().right > 750 || platform.getBounds().left < 0) {
+          platform.setVelocityX(-platform.body.velocity.x)
+        }
+      })
+}
